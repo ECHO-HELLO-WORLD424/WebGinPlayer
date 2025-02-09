5e92387621771d26490703d9a6b05c1c9622bf13
Patrick_Chen <patrickechohelloworld@outlook.com>
2025-02-09 10:36:48 +0800

    Better Project Structure for Scaling

--- main.go
+++ main.go
@@ -6,14 +6,68 @@ import (
 	"log"
 	"net/http"
 	"path/filepath"
+	"slices"
 	"time"
 )
 
+const (
+	// Music files
+	musicRelativePath = "/assets/music"
+	musicRootPath     = "./assets/music"
+	// Icon files
+	iconRelativePath = "/assets/icon"
+	iconRootPath     = "./assets/icon"
+	// Image files
+	imageRelativePath = "/assets/image"
+	imageRootPath     = "./assets/image"
+	// HTML pages
+	globalHTMLPattern = "templates/*.html"
+	playListPage      = "playlist.html"
+	// CSS & JS files
+	relativeCSSPath    = "/static/CSS"
+	rootCSSPath        = "./static/CSS"
+	relativePlaylistJS = "/static/JS/Playlist"
+	rootPlaylistJS     = "./static/JS/Playlist"
+)
+
 func main() {
+	expectedHosts := []string{
+		"127.0.0.1:8080",
+		"0.0.0.0:8080",
+	}
+	playlistRouter := createPlaylistRouter(expectedHosts)
+
+	// HTTP playlistServer
+	playlistServer := &http.Server{
+		Addr:         ":8080",
+		Handler:      playlistRouter,
+		ReadTimeout:  15 * time.Second,
+		WriteTimeout: 15 * time.Second,
+	}
+
+	err := playlistServer.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+}
+
+func createPlaylistRouter(expectedHost []string) *gin.Engine {
 	router := gin.Default()
 
-	// Disable caching for static files during development
 	router.Use(func(c *gin.Context) {
+		if !slices.Contains(expectedHost, c.Request.Host) {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "Invalid host header"})
+			return
+		}
+		// Security Headers
+		c.Header("X-Frame-Options", "DENY")
+		c.Header("Content-Security-Policy", "default-src 'self'; connect-src *; font-src *; script-src-elem * 'unsafe-inline'; img-src * data:; style-src * 'unsafe-inline';")
+		c.Header("X-XSS-Protection", "1; mode=block")
+		c.Header("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload")
+		c.Header("Referrer-Policy", "strict-origin")
+		c.Header("Permissions-Policy", "geolocation=(),midi=(),sync-xhr=(),microphone=(),camera=(),magnetometer=(),gyroscope=(),fullscreen=(self),payment=()")
+		// Disable caching for static files during development
 		c.Header("Cache-Control", "no-cache, no-store, must-revalidate")
 		c.Header("Pragma", "no-cache")
 		c.Header("Expires", "0")
@@ -21,13 +75,14 @@ func main() {
 	})
 
 	// Serve static files (HTML, CSS, JS)
-	router.Static("/static", "./static")
-	router.LoadHTMLGlob("templates/*")
+	router.Static(relativeCSSPath, rootCSSPath)
+	router.Static(relativePlaylistJS, rootPlaylistJS)
+	router.LoadHTMLGlob(globalHTMLPattern)
 
 	// Serve assets from the assets directory
-	router.Static("/assets/music", "./assets/music")
-	router.Static("/assets/icon", "./assets/icon")
-	router.Static("/assets/image", "./assets/image") // Add this line
+	router.Static(musicRelativePath, musicRootPath)
+	router.Static(iconRelativePath, iconRootPath)
+	router.Static(imageRelativePath, imageRootPath)
 
 	// Audio routes
 	router.GET("/", listAudioFile)
@@ -38,19 +93,7 @@ func main() {
 	router.POST("/upload/background", filemanager.UploadBackgroundImage)
 	router.GET("/background/current", filemanager.GetCurrentBackground)
 
-	// HTTP server
-	server := &http.Server{
-		Addr:         ":8080",
-		Handler:      router,
-		ReadTimeout:  15 * time.Second,
-		WriteTimeout: 15 * time.Second,
-	}
-
-	err := server.ListenAndServe()
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
+	return router
 }
 
 func listAudioFile(c *gin.Context) {
@@ -69,7 +112,7 @@ func listAudioFile(c *gin.Context) {
 		}
 	}
 
-	c.HTML(http.StatusOK, "index.html", gin.H{
+	c.HTML(http.StatusOK, playListPage, gin.H{
 		"musicFiles": musicFiles,
 	})
 }
